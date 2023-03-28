5cd7c7087e46f2da8a1d6aef45c318ae6349efc5
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-03-28 16:39:48 +0600

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     container: {
       center: true,
